35ef07071c89ada7bacac7e8d1ed66f7348b7171
Thorskii <utahsmile05@hotmail.com>
2022-08-05 22:44:49 -0600

    FIX JS

--- js/script.js
+++ js/script.js
@@ -2,17 +2,3 @@ import Swup from 'swup';
 const swup = new Swup();
 
 
-window.addEventListener('load', function () {
-    VANTA.FOG({
-    el: "#animation-bg",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    highlightColor: 0x7ddcfa,
-    midtoneColor: 0x4aa7ca,
-    lowlightColor: 0x406391
-    })
-})
-
